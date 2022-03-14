1d84845386a1ab6a157f0dc8790529ae0e23852b
Erkan Arı <erkan5355@gmail.com>
2022-03-14 14:27:09 +0300

    pojo

---
+++ src/test/java/get_http_reguest/PostReguestPojos01.java
@@ -0,0 +1,5 @@
+package get_http_reguest;
+
+public class PostReguestPojos01 {
+
+}
--- src/test/java/test_data/DummyTestData.java
+++ src/test/java/test_data/DummyTestData.java
@@ -1,5 +1,7 @@
 package test_data;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -94,9 +96,26 @@ public class DummyTestData {
                 expectedData.put("message", "Successfully! Record has been added.");
                 return expectedData;
 
-
-
-
-
             }
+
+    /*
+http://dummy.restapiexample.com/api/v1/delete/2 bir DELETE request gönderdiğimde
+
+Dönen response un status kodunun 200 ve body kısmının aşağıdaki gibi olduğunu test edin
+{
+"status": "success",
+"data": "2",
+"message": "Successfully! Record has been deleted"
+}
+*/
+    public JSONObject setUpDeleteExpectedData(){
+        JSONObject expectedData = new JSONObject();
+        expectedData.put("status", "success");
+        expectedData.put("data", "2");
+        expectedData.put("message", "Successfully! Record has been deleted");
+        return expectedData;
+    }
+
+
+
 }
